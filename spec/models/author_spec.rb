650c8e889525ad7653db4ba7d038f0d4d73d2c50
Anderson Rodrigues de Lima <arodrigues@mkmservice.com>
2021-02-06 13:39:59 -0300

    add spec model author

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -18,4 +18,31 @@ RSpec.describe Author, type: :model do
       it { expect{ described_class.create!(name: nil) }.to raise_error }
     end
   end
+
+  describe '#search' do
+    let(:subject) { described_class.search(filter_params, order) }
+    let(:order) { }
+    let!(:author1) { create(:author, name: 'Anderson') }
+    let!(:author2) { create(:author, name: 'Jackson') }
+    let!(:author3) { create(:author, name: 'Zack') }
+
+    context 'with same names end' do
+      let(:filter_params) { { 'name' => 'son' } }
+      it do
+        result = subject
+        expect(result.size).to eq(2)
+        expect(result.first.name).to eq(author1.name)
+        expect(result.second.name).to eq(author2.name)
+      end
+    end
+
+    context 'with only name' do
+      let(:filter_params) { { 'name' => 'za' } }
+      it do
+        result = subject
+        expect(result.size).to eq(1)
+        expect(result.first.name).to eq(author3.name)
+      end
+    end
+  end
 end
